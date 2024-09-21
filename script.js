1a0899510fbe2bb30b8e0fd0ec332bf7807f1d0f
MasumAhmed19 <masum.ahmed1328@gmail.com>
2024-09-21 21:46:50 +0600

    img compress and menu add

--- script.js
+++ script.js
@@ -14,28 +14,28 @@ const throttleFunction = (func, delay) => {
 }
 
 const imgarr = [
-    "images/img1.jpg",
-    "images/img2.jpg",
-    "images/img3.jpg",
-    "images/img4.jpg",
-    "images/img5.jpg",
-    "images/img6.jpg",
-    "images/img7.jpg",
-    "images/img8.jpg",
-    "images/img9.jpg",
-    "images/img10.jpg",
-    "images/img11.jpg",
-    "images/img12.jpg",
-    "images/img13.jpg",
-    "images/img14.jpg",
-    "images/img15.jpg",
-    "images/img16.jpg",
-    "images/img17.jpg",
-    "images/img18.jpg",
-    "images/img19.jpg",
-    "images/img20.jpg",
-    "images/img21.jpg",
-    "images/img22.jpg",
+    "images/img1.webp",
+    "images/img2.webp",
+    "images/img3.webp",
+    "images/img4.webp",
+    "images/img5.webp",
+    "images/img6.webp",
+    "images/img7.webp",
+    "images/img8.webp",
+    "images/img9.webp",
+    "images/img10.webp",
+    "images/img11.webp",
+    "images/img12.webp",
+    "images/img13.webp",
+    "images/img14.webp",
+    "images/img15.webp",
+    "images/img16.webp",
+    "images/img17.webp",
+    "images/img18.webp",
+    "images/img19.webp",
+    "images/img20.webp",
+    "images/img21.webp",
+    "images/img22.webp",
 ]
 
 
@@ -59,7 +59,7 @@ document.querySelector("#center").addEventListener("mousemove",
 
         // img coming with animation
         gsap.to(img, {
-            y: "0",
+            y: "-20%",
             ease: Power1,
             duration: 0.7
         })
